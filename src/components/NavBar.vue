68cc2d834713a7836af9414f5be90aac7c11a078
TimHarris96 <59544597+TimHarris96@users.noreply.github.com>
2023-12-03 10:39:45 +1100

    Router set up
    * Navbar
    * Home, About, Products and Order views
    * Logo

---
+++ src/components/NavBar.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+// import RouterLink from 'vue-router'
+</script>
+
+<template>
+  <div class="navbar">
+    <img
+      alt="Anomas Cakes logo"
+      class="logo"
+      src="@/assets/anomas_cakes_logo.jpg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/products">Products</RouterLink>
+        <RouterLink to="/order">Order</RouterLink>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>hello products</main>
+</template>
