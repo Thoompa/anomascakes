a8fb46625ccb061e4c320b98e81cdf888c7c80f3
TimHarris96 <59544597+TimHarris96@users.noreply.github.com>
2026-01-07 16:35:26 +1100

    Updated Cakes view

--- src/views/CakesView.vue
+++ src/views/CakesView.vue
@@ -1,15 +1,52 @@
 <template>
   <div class="menu">
-    <img alt="Anomas Cakes logo" class="images" src="@/assets/images/cakes.jpg" />
+    <v-row>
+      <v-col v-for="(item, i) in images" :key="i" cols="6" class="d-flex flex-column align-center">
+        <img
+          :src="item.img"
+          class="w-[400px] h-[400px] object-cover object-center rounded-lg mb-2"
+          :class="item.class"
+        />
+        <div class="text-h6 text-center">
+          {{ item.name }}
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const images = [
+  { name: 'Cupcakes', img: '/src/assets/images/cupcakes-square.jpg', class: 'square' },
+  { name: 'Small Cakes', img: '/src/assets/images/small-cakes-square.jpg', class: 'square' },
+  { name: 'Bento Cake Box', img: '/src/assets/images/bento-cake-box-square.jpg', class: 'square' },
+  { name: 'Sri Lankan Speciality Cakes', img: '/src/assets/images/sri-lankan-speciality-cakes-square.jpg', class: 'square' }
+]
+</script>
 
 <style scoped>
 .menu {
   display: flex;
   justify-content: center;
   margin-block: var(--default-margin-block);
+}
+
+.images {
+  width: 400px;
+}
+
+.top-row {
+  height: 311px;
+  width: 467px;
+}
+
+.btm-row {
+  height: 700px;
+  width: 467px;
+}
+
+.square {
+  height: 311px;
+  width: 311px;
 }
 </style>
